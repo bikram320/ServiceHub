6b1a61c17196f6829933986a3dd9407bf0cb3f3d
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-22 21:03:07 +0545

    name not null in table Users

--- src/main/resources/db/migration/V1__add_db_setUp.sql
+++ src/main/resources/db/migration/V1__add_db_setUp.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users (
     user_id    BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255),
+    name       VARCHAR(255) NOT NULL ,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     phone      VARCHAR(255) NOT NULL,
