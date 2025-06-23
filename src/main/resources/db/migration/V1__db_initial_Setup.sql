67a939b586bf3a1fc4944421579b15381d1f4503
Bikram Bishwokarma <bkbikram727@gmail.com>
2025-06-23 21:05:39 +0545

    Recreate the whole DataBase
    Recreated the whole database Schema ,
    due to error shown up while adding up new tables in the database .
    this is tentative final database schema

--- src/main/resources/db/migration/V1__db_initial_Setup.sql
+++ src/main/resources/db/migration/V1__db_initial_Setup.sql
@@ -5,12 +5,12 @@ CREATE TABLE Users (
     password   VARCHAR(255) NOT NULL,
     phone      VARCHAR(255) NOT NULL,
     address    VARCHAR(255) NOT NULL,
-    role       VARCHAR(50)  NOT NULL,
+    role       VARCHAR(50)  NOT NULL DEFAULT 'USER',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Technicians (
-    tech_id     INT AUTO_INCREMENT PRIMARY KEY,
+    tech_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
     email       VARCHAR(255) NOT NULL UNIQUE,
     password    VARCHAR(255) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE Technicians (
 CREATE TABLE ServiceRequests (
     request_id       BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id          BIGINT NOT NULL,
-    tech_id          INT NOT NULL,
+    tech_id          BIGINT NOT NULL,
     service_type     VARCHAR(255) NOT NULL,
     description      TEXT,
     appointment_time DATETIME NOT NULL,
@@ -36,4 +36,23 @@ CREATE TABLE ServiceRequests (
     created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES Users(user_id),
     CONSTRAINT fk_technician FOREIGN KEY (tech_id) REFERENCES Technicians(tech_id)
+);
+
+
+CREATE TABLE Admins (
+    admin_id   BIGINT PRIMARY KEY DEFAULT 1,
+    name       VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    role       VARCHAR(50) NOT NULL DEFAULT 'ADMIN'
+);
+
+CREATE TABLE AdminActions (
+    action_id     BIGINT AUTO_INCREMENT PRIMARY KEY,
+    admin_id      BIGINT NOT NULL,
+    action_type   VARCHAR(100) NOT NULL,
+    target_type   VARCHAR(100),
+    target_id     BIGINT,
+    description   TEXT,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_admin FOREIGN KEY (admin_id) REFERENCES Admins(admin_id)
 );
