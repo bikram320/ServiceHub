e048c47936148d710a3ea8ad6076241428000490
[Bikram Bk] <bkbikram727@gmail.com>
2025-09-18 15:55:46 +0545

    i don't know how it came

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,14 +12,14 @@ import AuthContainer from "./Components/layout/AuthContainer.jsx";
 
 function App() {
     return (
-       <Router>
-           <Routes>
-               <Route path="/" element={<LandingPage />} />
-               <Route path="/UserLayout/*" element={<UserLayout />} />
-               <Route path="/LoginSignup/*" element={<AnimatedAuth />} />
-           </Routes>
-       </Router>
-        // <Test />
+       // <Router>
+       //     <Routes>
+       //         <Route path="/" element={<LandingPage />} />
+       //         <Route path="/UserLayout/*" element={<UserLayout />} />
+       //         <Route path="/LoginSignup/*" element={<AnimatedAuth />} />
+       //     </Routes>
+       // </Router>
+         < AuthContainer/>
 
 
 );
