3fa362fb529c6d643b3772d0f9d1b69d57696744
[Bikram Bk] <bkbikram727@gmail.com>
2025-09-20 10:11:14 +0545

    user dashboard added in backend

--- Backend/src/main/java/org/example/QuestX/Repository/FeedbackRepository.java
+++ Backend/src/main/java/org/example/QuestX/Repository/FeedbackRepository.java
@@ -1,7 +1,10 @@
 package org.example.QuestX.Repository;
 
 import org.example.QuestX.Model.Feedback;
+import org.example.QuestX.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +13,9 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
     List<Feedback> findAllByRequest_Technician_Id(Long id);
 
     Optional<Feedback> findByRequest_Id(long id);
+
+
+    @Query("SELECT AVG(f.rating) FROM Feedback f WHERE f.request.user = :user")
+    Double getAverageRatingByUser(@Param("user") User user);
+
 }
